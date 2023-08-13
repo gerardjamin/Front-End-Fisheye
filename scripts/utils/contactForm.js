58c504c7fecfd33d8c2175eea51b19f272f7e81f
jamin <gerard.jamin@orange.fr>
2023-08-13 17:25:07 +0200

    dinamisme des champs du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -6,16 +6,31 @@ const nomInput = document.forms.formulaire.nom;
 const emailInput = document.forms.formulaire.email;
 const messageTextarea = document.forms.formulaire.message;
 
+//correction en temps reel du formulaire
+var formulaire = document.getElementById("signUp");
+var inputs = formulaire.getElementsByTagName("input");
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("blur", function(event) {
+        checkSurname()
+        checkName()
+        checkEmail()
+    });
+}
 
-
-//declaration des fonctions
-
+/**
+ * le prénom ne doit pas etre vide et avoir au moins 2 caractères sans chiffre
+ * @returns valid
+ */
 const checkSurname = () => {
-    let valid = false;
-    const surName = prenomInput.value.trim();
+    let valid = false
+    const surName = prenomInput.value
+    const regex = /^[A-Za-z][a-zA-ZÀ-ÖØ-öø-ÿ\s(--)]+$/
+    const estValide = regex.test(surName)
 
     if (!isRequired(surName)) {
-        showError(prenomInput, "Le prenom ne peut pas être vide");
+        showError(prenomInput, "Le prenom ne peut pas être vide")
+    } else if (!estValide) {
+        showError(nomInput, "Entrez un prénom valide")
     } else {
         showSuccess(prenomInput);
         valid = true;
@@ -23,18 +38,50 @@ const checkSurname = () => {
     return valid;
 }
 
+/**
+ * le nom de famille ne doit pas etre vide et avoir au moins  2 caractères sans chiffre
+ * @returns valid
+ */
 const checkName = () => {
     let valid = false;
-    const name = nomInput.value.trim();
+    const name = nomInput.value;
+    const regex = /^[A-Za-z][a-zA-ZÀ-ÖØ-öø-ÿ\s(--)]+$/;
+    const estValide = regex.test(name);
 
     if (!isRequired(name)) {
         showError(nomInput, "Le nom ne peut pas être vide");
-    } else {
+    } else if (!estValide) {
+        showError(nomInput, "Entrez un nom de famille valide");
+        } else  {
         showSuccess(nomInput);
         valid = true;
     }
     return valid;
 }
+
+/**
+ * utilisation d'une regex pour comparer le format type mail (ex:const resultat = /^coucou/.test(chaine))
+ * @returns valid
+ */
+const checkEmail = () => {
+    let valid = false;
+    const email = emailInput.value.trim();
+    if (!isRequired(email)) {
+        showError(emailInput, "l'email ne peut etre vide");
+    } else if (!isEmailValid(email)) {
+        showError(emailInput, "Email non valide");
+    } else {
+        showSuccess(emailInput);
+        valid = true;
+    }
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+    return emailRegExp.test(email); 
+};
+
 // Si la value retourne une chaine de caractére vide alors isRequired = false
 const isRequired = (value) => (value === "" ? false : true);
 
@@ -64,20 +111,21 @@ const showSuccess = (input) => {
 }
 
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", function (event) {
     // empécher l'envois du formulaire(le rechargement de la page)
-    e.preventDefault();
+    event.preventDefault();
   
     // validation des champs
     let isSurnameValid = checkSurname(),
-        isNameValid = checkName()
-
+        isNameValid = checkName(),
+        isEmailValid = checkEmail()
 
     let isFormValid =
         isSurnameValid &&
-        isNameValid 
+        isNameValid &&
+        isEmailValid
 
-        console.log(isFormValid)
+        return isFormValid
 })
 
 
